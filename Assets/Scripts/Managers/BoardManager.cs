19d225666b7df21faa82094843bae277c0976013
yunusburkut <yunus.burkut@outlook.com>
2025-02-04 13:22:04 +0300

    Texture deleted

--- Assets/Scripts/Managers/BoardManager.cs
+++ Assets/Scripts/Managers/BoardManager.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoardManager : MonoBehaviour
 {
+    [SerializeField] private Texture[] cardImages;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
