32269b5eaa97f6a95577df5edb60d69cceceabee
yunusburkut <yunus.burkut@outlook.com>
2025-02-09 06:04:53 +0300

    Buttons added

--- Assets/Scripts/InputTestManager.cs
+++ Assets/Scripts/InputTestManager.cs
@@ -1,49 +1,142 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class InputTestManager : MonoBehaviour
 {
-    private void Update()
+    [SerializeField] private InputAction drawAction;
+    [SerializeField] private InputAction suitSortAction;
+    [SerializeField] private InputAction numberSortAction;
+    [SerializeField] private InputAction smartSortAction;
+
+    // Safe area ayarları; bu değerleri Inspector'dan ayarlayabilirsiniz.
+    [SerializeField] private float safeAreaTopThreshold = 0.8f;   // Üst safe area başlangıcı (yüzde)
+    [SerializeField] private float safeAreaBottomThreshold = 0.3f; // Alt safe area bitişi (yüzde)
+
+    private Rect safeArea;
+
+    private void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Safe area bilgisini alıyoruz.
+        safeArea = Screen.safeArea;
+
+        // Input aksiyonlarını etkinleştiriyoruz.
+        drawAction.Enable();
+        suitSortAction.Enable();
+        numberSortAction.Enable();
+        smartSortAction.Enable();
+
+        // Callback'leri atıyoruz.
+        drawAction.performed += OnDrawPerformed;
+        suitSortAction.performed += OnSuitSortPerformed;
+        numberSortAction.performed += OnNumberSortPerformed;
+        smartSortAction.performed += OnSmartSortPerformed;
+    }
+
+    private void OnDisable()
+    {
+        // Callback'leri kaldırıyoruz.
+        drawAction.performed -= OnDrawPerformed;
+        suitSortAction.performed -= OnSuitSortPerformed;
+        numberSortAction.performed -= OnNumberSortPerformed;
+        smartSortAction.performed -= OnSmartSortPerformed;
+
+        // Input aksiyonlarını devre dışı bırakıyoruz.
+        drawAction.Disable();
+        suitSortAction.Disable();
+        numberSortAction.Disable();
+        smartSortAction.Disable();
+    }
+
+    
+    private void OnDrawPerformed(InputAction.CallbackContext context)
+    {
+        Vector2 pos = context.ReadValue<Vector2>();
+        ProcessInput(pos);
+    }
+
+    private void OnSuitSortPerformed(InputAction.CallbackContext context)
+    {
+        Vector2 pos = context.ReadValue<Vector2>();
+        ProcessInput(pos);
+    }
+
+    private void OnNumberSortPerformed(InputAction.CallbackContext context)
+    {
+        Vector2 pos = context.ReadValue<Vector2>();
+        ProcessInput(pos);
+    }
+
+    private void OnSmartSortPerformed(InputAction.CallbackContext context)
+    {
+        Vector2 pos = context.ReadValue<Vector2>();
+        ProcessInput(pos);
+    }
+
+    /// <summary>
+    /// Giriş pozisyonunu safe area içinde kontrol eder ve pozisyona göre ilgili aksiyonu tetikler.
+    /// Hem mobil hem PC girişleri için çalışır.
+    /// </summary>
+    /// <param name="inputPos">Giriş pozisyonu (dokunma veya fare)</param>
+    private void ProcessInput(Vector2 inputPos)
+    {
+        // Dokunmanın safe area içinde olup olmadığını kontrol ediyoruz.
+        if (!safeArea.Contains(inputPos))
+            return;
+
+        // Ekranın boyutlarını alalım.
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        // Örnek: Ekranın üst safe area bölgesi, safeArea'nın üst %20'si;
+        // alt safe area bölgesi, safeArea'nın alt %30'u.
+        if (inputPos.y >= safeArea.yMin + safeArea.height * safeAreaTopThreshold)
         {
+            // Üst safe area: 10 kart çiz (Space tuşuna denk).
             for (int i = 0; i < 10; i++)
             {
                 DrawAndLogCard();
             }
         }
-        if (Input.GetKeyDown(KeyCode.Q))
+        else if (inputPos.y <= safeArea.yMin + safeArea.height * safeAreaBottomThreshold)
         {
-            SuitSortTest();
+            // Alt safe area: Sol alt, orta alt, veya sağ alt bölgelere göre farklı aksiyonlar.
+            if (inputPos.x < safeArea.xMin + safeArea.width / 3f)
+            {
+                SuitSortTest();
+            }
+            else if (inputPos.x > safeArea.xMin + safeArea.width * 2f / 3f)
+            {
+                SmartSort();
+            }
+            else
+            {
+                NumberSortTest();
+            }
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        else
         {
-            NumberSortTest();
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            SmartSort();
+            // Orta bölgede isterseniz başka aksiyonlar tanımlayabilirsiniz.
         }
     }
 
-    public void OnDrawCardButtonPressed()
-    {
-        DrawAndLogCard();
-    }
-    private void SuitSortTest()
+    public void SuitSortTest()
     {
         MyDeckManager.Instance.CalculateSet();
     }
-    private void NumberSortTest()
+
+    public void NumberSortTest()
     {
-       MyDeckManager.Instance.CalculateRun();
+        MyDeckManager.Instance.CalculateRun();
     }
-    private void SmartSort()
-    { 
+
+    public void SmartSort()
+    {
         MyDeckManager.Instance.ComputeOptimalMelds();
     }
-    private void DrawAndLogCard()
+
+    public void DrawAndLogCard()
     {
-       BoardManager.Instance.DrawCard();
-       
+        BoardManager.Instance.DrawCard();
     }
 }
