91d77a85f17ad7a36118b50564755c5e2a5e581f
yunusburkut <yunus.burkut@outlook.com>
2025-02-10 06:16:58 +0300

    DONE wait for build

--- Assets/Scenes/LevelLoader.cs
+++ Assets/Scenes/LevelLoader.cs
@@ -4,32 +4,61 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    [Header("Sahne Ayarları")]
+    [Tooltip("Yüklenecek sahnenin build index değeri.")]
+    public int sceneIndex = 1;
+
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.enabled = false;
+    }
+
     public void StartGame()
     {
         StartCoroutine(StartAnim());
     }
 
+    public void StartUIAnimation()
+    {
+        animator.enabled = true;
+    }
+
     private IEnumerator StartAnim()
     {
+        StartUIAnimation();
         
         yield return null;
+        
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        
+        while (stateInfo.normalizedTime < 1.0f)
+        {
+            yield return null;
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        }
+        LoadLevel(sceneIndex);
     }
+
+    
     public void LoadLevel(int sceneIndex)
     {
         StartCoroutine(LoadAsynchronously(sceneIndex));
     }
 
+
     private IEnumerator LoadAsynchronously(int sceneIndex)
     {
         AsyncOperation asyncOp = SceneManager.LoadSceneAsync(sceneIndex);
-        
         asyncOp.allowSceneActivation = false;
         
         while (!asyncOp.isDone)
         {
             float progress = Mathf.Clamp01(asyncOp.progress / 0.9f);
             Debug.Log("Loading progress: " + (progress * 100f).ToString("F0") + "%");
-
+            
             if (asyncOp.progress >= 0.9f)
             {
                 yield return new WaitForSeconds(1f);
