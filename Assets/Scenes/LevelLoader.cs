42e5fa535ba4022916fb3a349985c0a77c7100a3
yunusburkut <yunus.burkut@outlook.com>
2025-02-10 03:23:59 +0300

    MainMenu eklendi

---
+++ Assets/Scenes/LevelLoader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    private IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation asyncOp = SceneManager.LoadSceneAsync(sceneIndex);
+        
+        asyncOp.allowSceneActivation = false;
+        
+        while (!asyncOp.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncOp.progress / 0.9f);
+            Debug.Log("Loading progress: " + (progress * 100f).ToString("F0") + "%");
+
+            if (asyncOp.progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(1f);
+                asyncOp.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
